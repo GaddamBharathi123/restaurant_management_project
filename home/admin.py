769c5691d78912410c8b7eb3fcde7c617f08d0a5
Gaddam Bharathi <gaddambharathi2005@gmail.com>
2025-08-07 09:46:58 +0530

    17910

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import MenuItem,Order 
 
-# Register your models here.
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('id','name','price')
+    search_fields = ('name',)
+    list_filter = ('price',)
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id','customer','total_amount','order_status','created_at')
+    list_filter = ('order_status','created_at')
+    search_fields = ('customer__useranme',)
+    filter_horizontal = ('order_items',)    
+
