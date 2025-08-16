a68bc4a7134213a646ecd11f712c101b584b4660
Gaddam Bharathi <gaddambharathi2005@gmail.com>
2025-08-16 22:03:19 +0530

    29104

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Menultem
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.conf import settings
 
 def homepage(request):
     menu_items = Menultem.objects.all()
@@ -12,5 +13,8 @@ def get_menu(request):
         {'name':'Paneer Butter Masala','description':'Cottage cheese cooked in creamy tomato gravy','price':180.00},
         {'name':'Chicken Biryani','descrition':'Basmati rice with spiced chicken','price':220.00},
         {'name':'Veg Fried Rice','descrition':'Fried rice with fresh vegetables and soy sauce','price':130.00},]
-    return Response(menu_data)        
-    
+    return Response(menu_data)
+
+def home_view(request):
+    restaurent_name=settings.RESTAURENT_NAME
+    return render(request,"home.html",{"restaurent_name":restaurent_name})
